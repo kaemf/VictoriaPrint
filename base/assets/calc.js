6abc1ad806d241f27ca8fc1d74a6866bc037729a
kaemf <magwman0417@gmail.com>
2025-04-03 23:59:10 +0300

    redesign, fix

--- base/assets/calc.js
+++ base/assets/calc.js
@@ -7,23 +7,27 @@ const multipliers = {
 }
 
 colorInput.addEventListener("input", () => {
-    const color = colorInput.value,
-        black = b_wInput.value;
+    const color = parseInt(colorInput.value) || 0,
+        black = parseInt(b_wInput.value) || 0;
 
     const out = color * multipliers.color,
         total = (color * multipliers.color) + (black * multipliers.b_w);
-    document.querySelector(".color > .data").textContent = `${out} грн`;
-    document.querySelector('.color > .title').textContent = `◆ (${color})КОЛІР:`;  
-    document.querySelector(".total-price > .data").textContent = `${total} грн`;
+
+    document.querySelector('.count > .color').textContent = color;
+    document.querySelector('.count > .total-value').textContent = color + black;
+    document.querySelector(".total-price > .color").textContent = `${out} грн`;
+    document.querySelector(".total-price > .total-value").textContent = `${total} грн`;
 })
 
 b_wInput.addEventListener("input", () => {
-    const color = colorInput.value,
-        black = b_wInput.value;
+    const color = parseInt(colorInput.value) || 0,
+        black = parseInt(b_wInput.value) || 0;
 
     const out = black * multipliers.b_w,
         total = (color * multipliers.color) + (black * multipliers.b_w);
-    document.querySelector(".b_w > .data").textContent = `${out} грн`;
-    document.querySelector('.b_w > .title').textContent = `◈ (${black})Ч/Б:`;
-    document.querySelector(".total-price > .data").textContent = `${total} грн`;
+
+    document.querySelector('.count > .black').textContent = black;
+    document.querySelector('.count > .total-value').textContent = color + black;
+    document.querySelector(".total-price > .black").textContent = `${out} грн`;
+    document.querySelector(".total-price > .total-value").textContent = `${total} грн`;
 })
